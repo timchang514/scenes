c110d4acb845b767c5c351ea8a769831bf1e9e11
Tim Chang <tim.chang@partner.samsung.com>
2017-06-23 16:39:21 -0400

    initial commit

--- src/js/scene.js
+++ src/js/scene.js
@@ -1,166 +1,95 @@
-
-			var container, stats;
-
-			var camera, scene, renderer;
-
-			var mesh;
-
-			var mouseX = 0, mouseY = 0;
-
-			var windowHalfX = window.innerWidth / 2;
-			var windowHalfY = window.innerHeight / 2;
-
-			init();
-			animate();
-
-			function init() {
-
-				container = document.getElementById( 'container' );
-
-				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
-				camera.position.z = 500;
-
-				scene = new THREE.Scene();
-
-				var data = generateHeight( 1024, 1024 );
-				var texture = new THREE.CanvasTexture( generateTexture( data, 1024, 1024 ) );
-				var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
-
-				var quality = 16, step = 1024 / quality;
-
-				var geometry = new THREE.PlaneGeometry( 2000, 2000, quality - 1, quality - 1 );
-				geometry.rotateX( - Math.PI / 2 );
-
-				for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
-
-					var x = i % quality, y = Math.floor( i / quality );
-					geometry.vertices[ i ].y = data[ ( x * step ) + ( y * step ) * 1024 ] * 2 - 128;
-
-				}
-
-				mesh = new THREE.Mesh( geometry, material );
-				scene.add( mesh );
-
-				renderer = new THREE.CanvasRenderer();
-				renderer.setClearColor( 0xbfd1e5 );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-				container.innerHTML = "";
-
-				container.appendChild( renderer.domElement );
-
-				stats = new Stats();
-				container.appendChild( stats.dom );
-
-				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-				//
-
-				window.addEventListener( 'resize', onWindowResize, false );
-
-			}
-
-			function onWindowResize() {
-
-				windowHalfX = window.innerWidth / 2;
-				windowHalfY = window.innerHeight / 2;
-
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-			}
-
-			function generateHeight( width, height ) {
-
-				var data = new Uint8Array( width * height ), perlin = new ImprovedNoise(),
-				size = width * height, quality = 2, z = Math.random() * 100;
-
-				for ( var j = 0; j < 4; j ++ ) {
-
-					quality *= 4;
-
-					for ( var i = 0; i < size; i ++ ) {
-
-						var x = i % width, y = ~~ ( i / width );
-						data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * 0.5 ) * quality + 10;
-
-					}
-
-				}
-
-				return data;
-
-			}
-
-			function generateTexture( data, width, height ) {
-
-				var canvas, context, image, imageData,
-				level, diff, vector3, sun, shade;
-
-				vector3 = new THREE.Vector3( 0, 0, 0 );
-
-				sun = new THREE.Vector3( 1, 1, 1 );
-				sun.normalize();
-
-				canvas = document.createElement( 'canvas' );
-				canvas.width = width;
-				canvas.height = height;
-
-				context = canvas.getContext( '2d' );
-				context.fillStyle = '#000';
-				context.fillRect( 0, 0, width, height );
-
-				image = context.getImageData( 0, 0, width, height );
-				imageData = image.data;
-
-				for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++  ) {
-
-					vector3.x = data[ j - 1 ] - data[ j + 1 ];
-					vector3.y = 2;
-					vector3.z = data[ j - width ] - data[ j + width ];
-					vector3.normalize();
-
-					shade = vector3.dot( sun );
-
-					imageData[ i ] = ( 96 + shade * 128 ) * ( data[ j ] * 0.007 );
-					imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( data[ j ] * 0.007 );
-					imageData[ i + 2 ] = ( shade * 96 ) * ( data[ j ] * 0.007 );
-
-				}
-
-				context.putImageData( image, 0, 0 );
-
-				return canvas;
-
-			}
-
-			function onDocumentMouseMove(event) {
-
-				mouseX = event.clientX - windowHalfX;
-				mouseY = event.clientY - windowHalfY;
-
-			}
-
-			//
-
-			function animate() {
-
-				requestAnimationFrame( animate );
-
-				render();
-				stats.update();
-
-			}
-
-			function render() {
-
-				camera.position.x += ( mouseX - camera.position.x ) * 0.05;
-				camera.position.y += ( - mouseY - camera.position.y ) * 0.05 + 15;
-				camera.lookAt( scene.position );
-
-				renderer.render( scene, camera );
-
-			}
+var container, stats
+var camera, scene, renderer
+var controls, group
+
+var water
+
+init()
+animate()
+
+function init() {
+	container = document.createElement('div')
+	document.body.appendChild(container)
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.5, 30000)
+	// (left/right rotation, up/down rotation,zoom)
+	camera.position.set( 2000, 750, 200 );
+	var controls = new THREE.OrbitControls(camera)
+	scene = new THREE.Scene()
+
+	// Skydome
+	renderer = new THREE.CanvasRenderer()
+	renderer.setClearColor(0xf0f0f0)
+	renderer.setPixelRatio(window.devicePixelRatio)
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	container.appendChild(renderer.domElement)
+
+	var skyGeo = new THREE.SphereGeometry(15000, 50, 50);		// Create a sphere
+	var skyLoader = new THREE.TextureLoader();
+	var skyMtl = new THREE.MeshPhongMaterial({							// Set the image and overdraw (to prevent extra lines being drawn)
+        map: skyLoader.load("src/img/sky-big.jpg"), overdraw: 0.5	// Note: not sure if we can use this image
+	});
+	var sky = new THREE.Mesh(skyGeo, skyMtl);								// Create sky mesh from geometry and material
+  sky.material.side = THREE.BackSide;											// Make sure we see texture from inside and not the outside
+  scene.add(sky);
+	//scene.fog = new THREE.FogExp2( 0xaabbbb, 0.00001 );
+
+	container = document.getElementById('container');
+	//
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+
+	controls.enablePan = false;
+	controls.minDistance = 1000.0;
+	controls.maxDistance = 5000.0;
+	controls.maxPolarAngle = Math.PI * 0.495;
+	controls.target.set( 0, 500, 0 );
+	scene.add( new THREE.AmbientLight( 0xFFFFFF ) );
+
+	// Light
+	var light = new THREE.DirectionalLight( 0x222222, 1 );
+	light.position.set( - 1, 1, - 1 );
+	scene.add( light );
+
+	var parameters = {
+		width: 2000,
+		height: 2000,
+		widthSegments: 250,
+		heightSegments: 250,
+		depth: 1500,
+		param: 4,
+		filterparam: 1
+	};
+	var waterNormals = new THREE.TextureLoader().load('examples/textures/waternormals.jpg');
+	waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+	water = new THREE.Water( renderer, camera, scene, {
+		textureWidth: 512,
+		textureHeight: 512,
+		waterNormals: waterNormals,
+		alpha: 	1.0,
+		sunDirection: light.position.clone().normalize(),
+		sunColor: 0xffffff,
+		waterColor: 0x68afb7,
+		distortionScale: 200.0,
+		fog: scene.fog != undefined
+	});
+	var mirrorMesh = new THREE.Mesh(
+		new THREE.PlaneBufferGeometry( parameters.width * 500, parameters.height * 500 ),
+		water.material
+	);
+	mirrorMesh.add( water );
+	mirrorMesh.rotation.x = - Math.PI * 0.5;
+	scene.add( mirrorMesh );
+}
+
+function animate() {
+	requestAnimationFrame(animate)
+	render()
+}
+
+function render() {
+	water.material.uniforms.time.value += 2.0 / 60.0;
+	water.render();
+	renderer.render( scene, camera );
+}
