b9f5e7300f486bce988544096c7199e056a291c2
Tim Chang <tim.chang@partner.samsung.com>
2017-07-03 10:17:02 -0400

    scrolling works

--- src/js/scene1-alt.js
+++ src/js/scene1-alt.js
@@ -15,14 +15,14 @@ animate()
 
 function init() {
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(75, 800/600, 0.1, 1000);
+	camera = new THREE.PerspectiveCamera(75, 400/300, 0.1, 1000);
 	camera.position.z = 50;
 	camera.position.y = 130;
 
 	//controls = new THREE.OrbitControls(camera)
 	renderer = new THREE.WebGLRenderer();
 	renderer.setClearColor(0xc4c4c4);
-	renderer.setSize(800, 600);
+	renderer.setSize(400, 300);
 	document.body.appendChild(renderer.domElement);
 	clock = new THREE.Clock();
 
@@ -40,32 +40,32 @@ function init() {
 		},
 		iMouse: {
 			type: 'v4',
-			value: new THREE.Vector2()
+			value: new THREE.Vector2(0.0,0.0)
 		}
 	};
 
+	var canvas = renderer.domElement;
+	var rect = canvas.getBoundingClientRect();
+
 	// Mouse position in - 1 to 1
 	renderer.domElement.addEventListener('mousedown', function(e) {
-		var canvas = renderer.domElement;
-		var rect = canvas.getBoundingClientRect();
-		tuniform.iMouse.value.x = (e.clientX - rect.left) / window.innerWidth * 2 - 1;
-		tuniform.iMouse.value.y = (e.clientY - rect.top) / window.innerHeight * -2 + 1;
+		tuniform.iMouse.value.x = (e.clientX - rect.left) / 400 * 2 - 1;
+		tuniform.iMouse.value.y = (e.clientY - rect.top) / 300 * -2 + 1;
 	});
 	renderer.domElement.addEventListener('mouseup', function(e) {
-		var canvas = renderer.domElement;
-		var rect = canvas.getBoundingClientRect();
-		tuniform.iMouse.value.z = (e.clientX - rect.left) / window.innerWidth * 2 - 1;
-		tuniform.iMouse.value.w = (e.clientY - rect.top) / window.innerHeight * -2 + 1;
-	});
-	// resize canvas function
-	window.addEventListener('resize',function() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
+		tuniform.iMouse.value.x = (e.clientX - rect.left) / 400 * 2 - 1;
+		tuniform.iMouse.value.y = (e.clientY - rect.top) / 300 * -2 + 1;
 	});
 
-	tuniform.iResolution.value.x = 800;
-	tuniform.iResolution.value.y = 600;
+	// resize canvas function
+	window.addEventListener('resize',function() {
+		camera.aspect = 400 / 300;
+		camera.updateProjectionMatrix();
+		renderer.setSize(400, 300);
+	});
+
+	tuniform.iResolution.value.x = 400;
+	tuniform.iResolution.value.y = 300;
 	// Create Plane
 	var material = new THREE.ShaderMaterial({
 		uniforms: tuniform,
@@ -73,10 +73,10 @@ function init() {
 		fragmentShader: document.getElementById('fragment-shader').textContent
 	});
 	water = new THREE.Mesh(
-		new THREE.PlaneBufferGeometry(800, 600, 40), material
+		new THREE.PlaneBufferGeometry(400, 300, 40), material
 	);
 	// water = new THREE.Mesh(
-	// 	new THREE.PlaneBufferGeometry(window.innerWidth, window.innerHeight, 40)
+	// 	new THREE.PlaneBufferGeometry(400, 300, 40)
 	// )
 	scene.add(water);
 }
@@ -89,6 +89,8 @@ function animate() {
 // draw animation
 function render(time) {
 	tuniform.iGlobalTime.value += clock.getDelta();
+	tuniform.iMouse.value.x += 0.1
+	// console.log(tuniform.iMouse.value.y)
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
 }
